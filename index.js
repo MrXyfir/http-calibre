53e90be0408d68f11bd5237d332c11a9667f7c6d
Mr. Xyfir <mr@xyfir.com>
2016-04-22 15:58:44 -0700

    Add middleware for validating library id and setting paths

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ app.listen(config.environment.port, () => {
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
+app.use("/library/:lib", function(req, res, next) {
+    if (!req.params.lib.match(/^A-Za-z0-9{40}$/)) {
+        res.send(1);
+    }
+    else {
+        req.path.lib = process.env.libdir + '/' + req.params.lib;
+        req.path.ul = process.env.uldir + '/' + req.params.lib;
+        
+        next();
+    }
+});
 app.use("/library/:lib", require("./controllers/"));
