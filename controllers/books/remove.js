42cf80e9f40790cecbc32c77433bb3032a90ddc5
Mr. Xyfir <mr@xyfir.com>
2016-04-22 16:46:01 -0700

    Add controller for DELETE library/:lib/books

---
+++ controllers/books/remove.js
@@ -0,0 +1,34 @@
+const exec = require("child_process").exec;
+const disk = require('diskusage');
+
+/*
+    DELETE library/:lib/books
+    REQUIRED
+        books: string
+    RETURN
+        { error: boolean, freeSpace?: number }
+    DESCRIPTION
+        Delete a list of books by id from library
+*/
+module.exports = function(req, res) {
+    
+    if (!req.body.books || !req.body.books.match(/^[0-9,]{1,}$/)) {
+        res.json({ error: true });
+        return;
+    }
+    
+    exec(
+        `calibredb remove ${req.body.books} --library-path ${req.path.lib} --dont-notify-gui`,
+        { cwd: process.env.calibredir }, (err, data, stderr) => {
+            if (err) {
+                res.json({ error: true });
+            }
+            else {
+                disk.check('/', (err, info) => {
+                    res.json({ error: false, freeSpace: info.free });
+                });
+            }
+        }
+    );
+    
+};
