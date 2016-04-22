7c49d94ba702712207923d1ec7da88bd8a3767e4
Mr. Xyfir <mr@xyfir.com>
2016-04-22 16:07:25 -0700

    Add controller for GET library/:lib/books

---
+++ controllers/books/list.js
@@ -0,0 +1,25 @@
+const exec = require("child_process").exec;
+
+/*
+    GET library/:lib/books
+    OPTIONAL
+        limit: number
+    RETURN
+        [{
+            author_sort: string, authors: string, cover: string, formats: string[],
+            id: number, rating: number, series: string, series_index: number,
+            tags: string[], title: string
+        }]
+    DESCRIPTION
+        Return basic info for books in library
+*/
+module.exports = function(req, res) {
+    
+    exec(
+        `calibredb list --library-path ${req.path.lib} --for-machine --fields author_sort,authors,cover,formats,id,rating,series,series_index,tags,title` + (req.query.limit ? ` --limit ${+req.query.limit}` : ''),
+        { cwd: process.env.calibredir }, (err, data, stderr) => {
+            res.send(err ? "[]" : data);
+        }
+    );
+    
+};
