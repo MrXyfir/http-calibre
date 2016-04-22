fc10861426ed0191c5885b0ce9b3c1701d342246
Mr. Xyfir <mr@xyfir.com>
2016-04-22 10:58:10 -0700

    Add routes

---
+++ controllers/index.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+
+/* Library */
+
+router.route("/")
+    .post(require("./library/create"))
+    .put(require("./library/move"))
+    .delete(require("./library/delete"));
+router.post("/upload", require("./upload/library"));
+
+/* Books */
+
+router.route("/books")
+    .get(require("./books/list"))
+    .post(require("./books/add"))
+    .delete(require("./books/remove"));
+router.get("/books/search", require("./books/search"));
+router.post("/books/upload", require("./upload/books"));
+    
+router.route("/books/:book/metadata")
+    .get(require("./metadata/get"))
+    .put(require("./metadata/set"))
+    .patch(require("./metadata/update"));
+    
+router.route("/books/:book/format/:format")
+    .post(require("./format/add"))
+    .delete(require("./format/remove"));
+router.post("/books/:book/format/:format/convert", require("./format/convert-to"));
+
+module.exports = router;
