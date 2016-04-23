ed784edd67585b627c7c5357efd81fcd38f27f45
Mr. Xyfir <mr@xyfir.com>
2016-04-22 18:01:15 -0700

    Add controller for GET library/:lib/books/search

---
+++ controllers/books/search.js
@@ -0,0 +1,22 @@
+const escape = require("js-string-escape");
+const exec = require("child_process").exec;
+
+/*
+    GET library/:lib/books/search
+    REQUIRED
+        search: string
+    RETURN
+        number[]
+    DESCRIPTION
+        Return ids of books that match search query
+*/
+module.exports = function(req, res) {
+    
+    exec(
+        `calibredb search "${escape(query)}" --library-path ${req.path.lib}`,
+        { cwd: process.env.calibredir }, (err, data, stderr) => {
+            res.json(err ? [] : data.split(','));
+        }
+    );
+    
+};
