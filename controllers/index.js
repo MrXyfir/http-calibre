d5f3ede78c2a0d6493a1b7a16316e5796b0bf574
Mr. Xyfir <mr@xyfir.com>
2016-04-23 18:48:21 -0700

    Implement multer for file uploads

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,17 @@
 const router = require("express").Router();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, req.path.ul)
+  },
+  filename: function(req, file, cb) {
+    cb( null, Date.now().toString().substr(-4) + '-' + file.originalname);
+  }
+})
+
+const uploadBooks = multer({storage, limits: {fileSize: 5000001, files: 20}});
+const uploadLibrary = multer({storage, limits: {fileSize: 500000001, files: 1}})
 
 /* Library */
 
@@ -6,13 +19,13 @@ router.route("/")
     .post(require("./library/create"))
     .put(require("./library/move"))
     .delete(require("./library/delete"));
-router.post("/upload", require("./library/upload"));
+router.post("/upload", uploadLibrary.single("lib"), require("./library/upload"));
 
 /* Books */
 
 router.route("/books")
     .get(require("./books/list"))
-    .post(require("./books/add"))
+    .post(uploadBooks.array("books", 20), require("./books/add"))
     .delete(require("./books/remove"));
 router.get("/books/search", require("./books/search"));
     
@@ -20,7 +33,7 @@ router.route("/books/:book/metadata")
     .get(require("./metadata/get"))
     .put(require("./metadata/set"));
 
-router.post("/books/:book/format/", require("./format/add"));
+router.post("/books/:book/format/", uploadBooks.single("book"), require("./format/add"));
 router.delete("/books/:book/format/:format", require("./format/remove"));
 router.post("/books/:book/format/convert/:from/:to", require("./format/convert-to"));
 
