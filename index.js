61ceab360e2399bef5e7d474a496737e504b6761
Mr. Xyfir <mr@xyfir.com>
2016-04-25 16:30:51 -0700

    Change res.send -> res.json, req.path -> req._path

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ app.use(parser.urlencoded({ extended: true }));
 
 app.use("/library/:lib", function(req, res, next) {
     if (!req.params.lib.match(/^[A-Za-z0-9]{40}$/)) {
-        res.send(1);
+        res.json({ error: true });
     }
     else {
-        req.path.lib = process.env.libdir + '/' + req.params.lib;
-        req.path.ul = process.env.uldir + '/' + req.params.lib;
+        req._path = {
+            lib: process.env.libdir + '/' + req.params.lib,
+            ul: process.env.uldir + '/' + req.params.lib
+        };
         
         next();
     }
