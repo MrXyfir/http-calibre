71ebbf74dd3d3476bf2316e90e0e57738f2eff82
Mr. Xyfir <mr@xyfir.com>
2016-04-27 09:35:51 -0700

    Add controller for GET library/:lib/size

---
+++ controllers/library/size.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const getFolderSize = require("get-folder-size");
+
+/*
+    GET library/:lib/size
+    RETURN
+        { error: boolean, size?: number }
+    DESCRIPTION
+        Returns a library's size in bytes
+*/
+module.exports = function(req, res) {
+    
+    getFolderSize(req._path.lib, (err, size) => {
+        if (err)
+            res.json({ error: true });
+        else
+            res.json({ error: false, size });
+    });
+    
+};
