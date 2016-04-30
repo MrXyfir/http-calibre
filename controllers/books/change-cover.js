0043ef28c23eca918b35ad12c7acc0d1e2435789
Mr. Xyfir <mr@xyfir.com>
2016-04-29 17:04:30 -0700

    Add controller for PUT library/:lib/books/:book/cover

---
+++ controllers/books/change-cover.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const sqlite = require("sqlite3");
+const exec = require("child_process").exec;
+const disk = require("diskusage");
+const fs = require("fs");
+
+/*
+    PUT library/:lib/books/:book/cover
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Updates a book's cover with uploaded image
+        Calls main API to update book's cover version + server's free space
+*/
+module.exports = function(req, res) {
+    
+    const db = new sqlite.Database(req._path.lib + "/metadata.db");
+    
+    // Grab book's path within library
+    db.get("SELECT path FROM books WHERE id = ?", [+req.params.book], (err, row) => {
+        db.close();
+        
+        if (err || !row) {
+            res.json({ error: true });
+        }
+        else {
+            const path = req._path.lib + '/' + row.path + "/cover.jpg";
+            
+            // Overwrite old cover image
+            fs.rename(req.file.path, path, err => {
+                if (err) {
+                    res.json({ error: true });
+                }
+                else {
+                    exec(
+                        `calibredb embed_metadata --library-path ${req._path.lib} --dont-notify-gui ${+req.params.book}`,
+                        { cwd: process.env.calibredir }, (err, data, stderr) => {
+                            res.json({ error: false });
+                            
+                            disk.check(process.env.rootdir, (err, info) => {
+                                request.put({
+                                    url: process.env.apiurl + req.params.lib + "/books/" + +req.params.book,
+                                    form: { type: "cover", freeSpace: info.free }
+                                }, (err, response, body) => {
+                                    
+                                });
+                            });
+                        }
+                    );
+                }
+            });
+        }
+    });
+    
+};
