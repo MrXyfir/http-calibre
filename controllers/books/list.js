73c8857c6f0d4d52b1ae9106fabed37781149140
Mr. Xyfir <mr@xyfir.com>
2016-05-16 11:01:21 -0700

    Output more metadata fields

--- controllers/books/list.js
+++ controllers/books/list.js
@@ -9,16 +9,18 @@ const exec = require("child_process").exec;
     RETURN
         { books: [{
             author_sort: string, authors: string, cover: string, formats: string[],
+			tags: string[], title: string, pubdate: string, publisher: string,
             id: number, rating: number, series: string, series_index: number,
-            tags: string[], title: string
+            last_modified: string, identifiers: string, size: number,
+            timestamp: number, comments: string
         }]}
     DESCRIPTION
-        Return basic info for books in library
+        Return metadata / info for books in library
 */
 module.exports = function(req, res) {
     
     exec(
-        `calibredb list --library-path ${req._path.lib} --for-machine --fields author_sort,authors,cover,formats,id,rating,series,series_index,tags,title` + (req.query.limit ? ` --limit ${+req.query.limit}` : ''),
+        `calibredb list --library-path ${req._path.lib} --for-machine --fields author_sort,authors,cover,formats,id,rating,series,series_index,tags,title,pubdate,publisher,last_modified,identifiers,size,timestamp,comments` + (req.query.limit ? ` --limit ${+req.query.limit}` : ''),
         { cwd: process.env.calibredir }, (err, data, stderr) => {
             res.json({
                 books: err ? [] : JSON.parse(data)
