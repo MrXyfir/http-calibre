74bc12b8717bedcf764fcda85d5d59b37a4e43d6
MrXyfir <mr@xyfir.com>
2016-09-23 11:45:15 -0700

    Hide full file paths in formats[]/cover

--- controllers/books/list.js
+++ controllers/books/list.js
@@ -20,9 +20,23 @@ module.exports = function(req, res) {
     exec(
         `calibredb list --library-path ${req._path.lib} --for-machine --fields author_sort,authors,cover,formats,id,rating,series,series_index,tags,title,pubdate,publisher,last_modified,identifiers,size,timestamp,comments` + (req.query.limit ? ` --limit ${+req.query.limit}` : ''),
         (err, data, stderr) => {
-            res.json({
-                books: err ? [] : JSON.parse(data)
-            });
+            if (err) {
+                res.json({ books: [] });
+            }
+            else {
+                // Ensure books[i].cover|formats paths are only
+                // author_folder/book_folder/file and not full file paths
+                res.json({
+                    books: JSON.parse(data).map(book => {
+                        return Object.assign({}, book, {
+                            cover: book.cover.split('/').slice(-3).join('/'),
+                            formats: book.formats.map(format => {
+                                return format.split('/').slice(-3).join('/');
+                            })
+                        });
+                    })
+                });
+            }
         }
     );
     
