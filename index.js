69a0ac0af1bef344421540146959775739a99a8e
MrXyfir <mr@xyfir.com>
2017-02-08 13:46:28 -0800

    Decrease tab spacing size to 2. Change " to '

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-require("app-module-path").addPath(__dirname);
+require('app-module-path').addPath(__dirname);
 
-const express = require("express");
-const parser = require("body-parser");
+const express = require('express');
+const parser = require('body-parser');
 
-const config = require("config");
+const config = require('config');
 const app = express();
 
-if (config.environment.type == "dev") {
-	app.use(require("cors")());
+if (config.environment.type == 'dev') {
+  app.use(require('cors')());
 }
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-app.use(["/files/:lib", "/:lib"], function(req, res, next) {
-    if (!req.params.lib.match(/^[0-9]{1,10}-[A-Za-z0-9]{40}$/)) {
-        res.json({ error: true });
-    }
-    else {
-        req._path = {
-            lib: config.directories.libraries + '/' + req.params.lib,
-            ul: config.directories.uploads + '/' + req.params.lib
-        };
-        req._libId = req.params.lib;
-        
-        next();
-    }
+app.use(['/files/:lib', '/:lib'], function(req, res, next) {
+  if (!req.params.lib.match(/^[0-9]{1,10}-[A-Za-z0-9]{40}$/)) {
+    res.json({ error: true });
+  }
+  else {
+    req._path = {
+      lib: config.directories.libraries + '/' + req.params.lib,
+      ul: config.directories.uploads + '/' + req.params.lib
+    };
+    req._libId = req.params.lib;
+    
+    next();
+  }
 });
-app.use("/files/:lib", require("./controllers/files/"));
-app.use("/:lib", require("./controllers/"));
+app.use('/files/:lib', require('./controllers/files/'));
+app.use('/:lib', require('./controllers/'));
 
 app.listen(config.environment.port, () => {
-    console.log("~~Server running on port", config.environment.port);
+  console.log('~~Server running on port', config.environment.port);
 });
