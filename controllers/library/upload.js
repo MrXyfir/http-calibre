a40ea6ccaa199753628210af51d350f54ccf788c
MrXyfir <mr@xyfir.com>
2017-02-08 15:07:36 -0800

    Improvements
    - Decrease tab spacing to 2 spaces
    - Replace " with '
    - Replace request with superagent
    - Clean up code

--- controllers/library/upload.js
+++ controllers/library/upload.js
@@ -1,72 +1,56 @@
-const resizeDisk = require("lib/resize-disk");
-const request = require("request");
-const sqlite = require("sqlite3");
-const escape = require("js-string-escape");
-const unzip = require("extract-zip");
-const exec = require("child_process").exec;
-const fs = require("fs-extra");
+const resizeDisk = require('lib/resize-disk');
+const request = require('superagent');
+const sqlite = require('sqlite3');
+const unzip = require('extract-zip');
+const fs = require('fs-extra');
 
-const config = require("config");
+const config = require('config');
 
 /*
-    POST :lib/upload
-    RETURN
-        { error: boolean, message?: string }
-    DESCRIPTION
-        Empty previous library directory
-		Unzip file into library folder
-		Grab all book ids from metadata.db
-		Send book ids to core API
+  POST :lib/upload
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Empty previous library directory
+    Unzip file into library folder
+    Grab all book ids from metadata.db
+    Send book ids to core API
 */
 module.exports = function(req, res) {
-    
-    fs.emptyDir(req._path.lib, err => {
-        if (err) {
-            res.json({
-                error: true, message: "Could not wipe old library"
-            });
-        }
-        else {
-            unzip(req.file.path, { dir: req._path.lib }, (err) => {
-                if (err) {
-                    res.json({
-                        error: true, message: "Could not unzip file"
-                    });
-                }
-                else {
-                    const db = new sqlite.Database(
-                        req._path.lib + "/metadata.db"
-                    );
-            
-                    db.all("SELECT id FROM 'books'", (err, rows) => {
-                        db.close();
 
-                        if (err || !rows.length) {
-                            res.json({
-                                error: true, message: "Invalid library"
-                            });
-                        }
-                        else {
-                            const ids = rows.map(row => row.id).join(',');
-                            
-                            fs.emptyDir(req._path.ul, err => {
-                                request.post({
-                                    url: config.urls.api + req._libId
-                                        + "/library",
-                                    form: { ids }
-                                }, (err, response, body) => {
-                                    res.json({
-                                        error: !!err || JSON.parse(body).error
-                                    });
-                                    
-                                    resizeDisk();
-                                });
-                            });
-                        }
-                    });
-                }
-            });
-        }
-    });
+  const error = err => {
+    res.json({ error: true, message: err });
+  };
+  
+  fs.emptyDir(req._path.lib, err => {
+    if (err) error('Could not wipe old library'); return;
     
+    unzip(req.file.path, { dir: req._path.lib }, (err) => {
+      if (err) error('Could not unzip file'); return;
+
+      const db = new sqlite.Database(
+        req._path.lib + '/metadata.db'
+      );
+  
+      db.all('SELECT id FROM "books"', (err, rows) => {
+        db.close();
+
+        if (err || !rows.length) error('Invalid library'); return;
+        
+        const ids = rows.map(row => row.id).join(',');
+        
+        request
+          .post(config.urls.api + req._libId + '/library')
+          .send({ ids })
+          .end((err, result) => {
+            if (err || result.body.error)
+              error('Could not add books to xyBooks'); return;
+            
+            fs.emptyDir(req._path.ul, () => 1);
+            resizeDisk();
+          });
+      });
+    });
+  });
+  
 };
