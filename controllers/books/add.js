ac31a643a3f8dd941ff853226efcfb87669f2675
MrXyfir <mr@xyfir.com>
2017-02-08 13:47:15 -0800

    Improvements
    - Decrease tab spacing size to 2
    - Replace " with '
    - Use Calibre class
    - Replace request with superagent

--- controllers/books/add.js
+++ controllers/books/add.js
@@ -1,62 +1,64 @@
-const resizeDisk = require("lib/resize-disk");
-const request = require("request");
-const escape = require("js-string-escape");
-const exec = require("child_process").exec;
-const fs = require("fs-extra");
+const resizeDisk = require('lib/resize-disk');
+const request = require('superagent');
+const Calibre = require('lib/Calibre');
+const fs = require('fs-extra');
 
-const config = require("config");
+const config = require('config');
 
 /*
-    POST :lib/books
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Add uploadeded books to library
-        Notify core API of new book(s)
+  POST :lib/books
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Add uploadeded books to library
+    Notify core API of new book(s)
 */
 module.exports = function(req, res) { 
-    
-    const files = req.files.map(file => {
-        return `"${escape(file.path)}" `;
-    }).join(' ');
-    
-    exec(
-        `calibredb add --library-path ${req._path.lib} --dont-notify-gui ${files}`,
-        (err, data, stderr) => {
-            if (err || data.indexOf("Added book ids:") == -1) {
-                fs.emptyDir(req._path.ul, err => {
-                    res.json({ error: true });
-                });
+  
+  const calibre = new Calibre(req._path.lib),
+  files = req.files.map(file => file.path);
+
+  let removeBooks = false, ids = '';
+
+  calibre.run('calibredb add', files)
+    .then(result => {
+      if (result.indexOf('Added book ids:') == -1)
+        throw 'Could not add book(s)';
+
+      ids = result
+        .split('Added book ids: ')[1]
+        .replace(new RegExp('[^0-9,]', 'g'), '');
+      
+      // Notify Xyfir Books of new books
+      return new Promise((resolve, reject) => {
+        request
+          .post(config.urls.api + req._libId + '/books')
+          .send({ ids })
+          .end((err, response) => {
+            // Error adding books to Xyfir Books
+            if (err || response.body.error) {
+              removeBooks = true;
+              reject('Could not add book to xyBooks');
             }
             else {
-                const ids = data.split("Added book ids: ")[1]
-                    .replace(new RegExp("[^0-9,]", 'g'), '');
-                
-                // Notify Xyfir Books of new books
-                fs.emptyDir(req._path.ul, err => {
-                    request.post({
-                        url: config.urls.api + req._libId + "/books",
-                        form: { ids }
-                    }, (err, response, body) => {
-                        // Error adding books to Xyfir Books
-                        if (err || JSON.parse(body).error) {
-                            res.json({ error: true });
-
-                            // Delete newly added books
-                            exec(
-                                `calibredb remove --library-path ${req._path.lib} --dont-notify-gui ${ids}`,
-                                (err, response, body) => 1
-                            );
-                        }
-                        // Xyfir Books accepted new books
-                        else {
-                            res.json({ error: false });
-                            resizeDisk();
-                        }
-                    });
-                });
+              resolve();
             }
-        }
-    )
-    
+          });
+      });
+    })
+    .then(() => {
+      res.json({ error: false });
+      
+      fs.emptyDir(req._path.ul, () => 1);
+      resizeDisk();
+    })
+    .catch(err => {
+      res.json({ error: true, message: err });
+      
+      fs.emptyDir(req._path.ul, () => 1);
+
+      // Delete newly added books
+      if (removeBooks) calibre.run('calibredb remove', [ids]);
+    });
+  
 };
