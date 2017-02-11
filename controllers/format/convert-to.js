36855901d4a67476ede64ee0534f23095099565d
MrXyfir <mr@xyfir.com>
2017-02-10 17:19:21 -0800

    Use Calibre class. Clean up code. Change coding convention. Change route

--- controllers/format/convert-to.js
+++ controllers/format/convert-to.js
@@ -1,83 +1,83 @@
-const resizeDisk = require("lib/resize-disk");
-const request = require("request");
-const sqlite = require("sqlite3");
-const escape = require("js-string-escape");
-const exec = require("child_process").exec;
-const fs = require("fs-extra");
+const resizeDisk = require('lib/resize-disk');
+const Calibre = require('lib/Calibre');
+const request = require('superagent');
+const sqlite = require('sqlite3');
+const fs = require('fs-extra');
 
 /*
-    POST :lib/books/:book/format/convert/:from/:to
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Gets path of :from for :book from Calibre's metadata.db
-        Attempts to generate a :to format version of :book using :from
-        Adds newly generated version to library
+  POST :lib/books/:book/format/convert
+  REQUIRED
+    from: string, to: string
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Gets path of q.from for :book from Calibre's metadata.db
+    Attempts to generate a q.to format version of :book using q.from
+    Adds newly generated version to library
 */
 module.exports = function(req, res) {
-    
-    if (!req.params.from.match(/^[A-Za-z0-9]{1,5}$/) || !req.params.to.match(/^[A-Za-z0-9]{1,5}$/)) {
-        res.json({ error: true });
-        return;
+
+  const regex = /^[A-Za-z0-9]{1,5}$/;
+  
+  if (!regex.test(req.query.from) || !regex.test(req.query.to)) {
+    res.json({ error: true, message: 'Invalid format type(s)' });
+    return;
+  }
+  
+  const db = new sqlite.Database(req._path.lib + '/metadata.db');
+  let path = '', sql = '', vars = [];
+  
+  // Get book path from books table WHERE id
+  sql = 'SELECT path FROM "books" WHERE id = ?',
+  vars = [+req.params.book];
+  
+  db.get(sql, vars, (err, row) => {
+    if (err || !row) {
+      db.close();
+      res.json({ error: true, message: 'Could not find book' });
+      return;
     }
     
-    const db = new sqlite.Database(req._path.lib + "/metadata.db");
-    let path = "", sql = "", vars = [];
+    path = req._path.lib + '/' + row.path + '/';
     
-    // Get book path from books table WHERE id
-    sql = "SELECT path FROM 'books' WHERE id = ?";
-    vars = [+req.params.book];
+    // Get book file name from data table WHERE id AND format
+    sql = 'SELECT name FROM "data" WHERE book = ? AND format = ?';
+    vars = [+req.params.book, req.query.from];
     
     db.get(sql, vars, (err, row) => {
-        if (err || !row) {
-            db.close();
-            res.json({ error: true });
-        }
-        else {
-            path = req._path.lib + '/' + row.path + '/';
-            
-            // Get book file name from data table WHERE id AND format
-            sql = "SELECT name FROM 'data' WHERE book = ? AND format = ?";
-            vars = [+req.params.book, req.params.from];
-            
-            db.get(sql, vars, (err, row) => {
-                db.close();
-                
-                if (err || !row) {
-                    res.json({ error: true });
-                }
-                else {
-                    const nPath = path + row.name + '.' + req.params.to;
-                    path += row.name + '.' + req.params.from; 
-                    
-                    // Attempt to convert to new format
-                    exec(
-                        `ebook-convert "${escape(path)}" "${escape(nPath)}"`,
-                        (err, data, stderr) => {
-                            if (err || data.indexOf("Output saved to") == -1) {
-                                res.json({ error: true });
-                            }
-                            else {
-                                // Add format to :book
-                                exec(
-                                    `calibredb add_format --library-path ${req._path.lib} --dont-notify-gui ${+req.params.book} "${escape(nPath)}"`,
-                                    (err, data, stderr) => {
-                                        if (err || data.indexOf("Error") != -1) {
-                                            res.json({ error: true });
-                                        }
-                                        else {
-                                            res.json({ error: false });
- 
-                                            resizeDisk();
-                                        }
-                                    }
-                                )
-                            }
-                        }
-                    );
-                }
-            });
-        }
+      db.close();
+      
+      if (err || !row) {
+        res.json({ error: true, message: 'Could not find format' });
+        return;
+      }
+      
+      // Build paths
+      const nPath = path + row.name + '.' + req.query.to;
+      path += row.name + '.' + req.query.from; 
+
+      const calibre = new Calibre(req._path.lib);
+
+      // Convert file at path to new file at nPath
+      calibre.run('ebook-convert', [path, nPath])
+        .then(result => {
+          if (result.indexOf('Output saved to') == -1)
+            throw 'Could not convert format';
+          
+          // Add new format at nPath to book at req.params.book
+          return calibre.run('add_format', [+req.params.book, nPath]);
+        })
+        .then(result => {
+          if (result.indexOf('Error') > -1)
+            throw 'Could not add format to library';
+          
+          res.json({ error: false });
+          resizeDisk();
+        })
+        .catch(err => {
+          res.json({ error: true, message: err });
+        });
     });
-    
+  });
+  
 };
