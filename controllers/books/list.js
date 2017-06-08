9aaa7ce2b75e9b44d73647ccaa8205c236b7a345
MrXyfir <mr@xyfir.com>
2017-06-07 20:47:15 -0700

    Add default values

--- controllers/books/list.js
+++ controllers/books/list.js
@@ -37,16 +37,30 @@ module.exports = function(req, res) {
 
   calibre.on('close', code => {
     if (code == 0) {
-      // Ensure books[i].cover|formats paths are only
-      // author_folder/book_folder/file and not full file paths
-      output = JSON.parse(output).map(book =>
-        Object.assign(book, {
-          cover:
-            book.cover.split('/').slice(-3).join('/'),
-          formats:
-            book.formats.map(format => format.split('/').slice(-3).join('/'))
+      const defaults = {
+        cover: String, rating: Number, series: String, publisher: String,
+        identifiers: String, comments: String
+      };
+
+      output = JSON.parse(output)
+        // Add default values to fields that may not be returned by Calibre
+        .map(book => {
+          Object.keys(defaults).forEach(k =>
+            book[k] = book[k] || defaults[k]()
+          );
+
+          return book;
         })
-      );
+        // Ensure books[i].cover|formats paths are only
+        // author_folder/book_folder/file and not full file paths
+        .map(book =>
+          Object.assign(book, {
+            cover:
+              book.cover.split('/').slice(-3).join('/'),
+            formats:
+              book.formats.map(format => format.split('/').slice(-3).join('/'))
+          })
+        );
     }
     else {
       output = [];
