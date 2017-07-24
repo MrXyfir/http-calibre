f7747fa0254c0454f6753ffb706c9ff1c4d8918d
MrXyfir <mr@xyfir.com>
2017-07-24 13:55:35 -0700

    Rename; support /downloads route; minor changes

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const parser = require('body-parser');
 const config = require('config');
 const app = express();
 
-if (config.environment.type == 'dev') {
-  app.use(require('cors')());
-}
+if (config.environment.type == 'dev') app.use(require('cors')());
+if (config.environment.runCronJobs) require('./jobs/start-cron')();
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-app.use(['/files/:lib', '/:lib'], function(req, res, next) {
+app.use(['/files/:lib', '/:lib'], (req, res, next) => {
   if (!req.params.lib.match(/^[0-9]{1,10}-[A-Za-z0-9]{40,128}$/)) {
     res.json({ error: true, message: 'Invalid library id' });
   }
@@ -21,7 +20,7 @@ app.use(['/files/:lib', '/:lib'], function(req, res, next) {
     req._path = {
       lib: config.directories.libraries + '/' + req.params.lib,
       ul: config.directories.uploads + '/' + req.params.lib
-    };
+    },
     req._libId = req.params.lib;
     
     next();
@@ -30,6 +29,12 @@ app.use(['/files/:lib', '/:lib'], function(req, res, next) {
 app.use('/files/:lib', require('./controllers/files/'));
 app.use('/:lib', require('./controllers/'));
 
-app.listen(config.environment.port, () => {
-  console.log('~~Server running on port', config.environment.port);
-});
+app.get('/downloads/:file', (req, res) =>
+  req.params.file.test(/^\w+_\d+$/) ?
+  res.sendFile(config.directories.downloads + '/' + req.params.file) :
+  res.status(404).send()
+);
+
+app.listen(config.environment.port, () =>
+  console.log('~~Server running on port', config.environment.port)
+);
