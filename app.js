9410e036e8cddd264d4f73e0ee9d3619d62abf24
MrXyfir <mr@xyfir.com>
2017-07-24 19:23:50 -0700

    Move :lib/ route to libraries/:lib/. Move downloads controller

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (config.environment.runCronJobs) require('./jobs/start-cron')();
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-app.use(['/files/:lib', '/:lib'], (req, res, next) => {
+app.use(['/files/:lib', '/libraries/:lib'], (req, res, next) => {
   if (!req.params.lib.match(/^[0-9]{1,10}-[A-Za-z0-9]{40,128}$/)) {
     res.json({ error: true, message: 'Invalid library id' });
   }
@@ -27,13 +27,8 @@ app.use(['/files/:lib', '/:lib'], (req, res, next) => {
   }
 });
 app.use('/files/:lib', require('./controllers/files/'));
-app.use('/:lib', require('./controllers/'));
-
-app.get('/downloads/:file', (req, res) =>
-  req.params.file.test(/^\w+_\d+$/) ?
-  res.sendFile(config.directories.downloads + '/' + req.params.file) :
-  res.status(404).send()
-);
+app.use('/libraries/:lib', require('./controllers/'));
+app.get('/downloads/:file', require('./controllers/downloads'));
 
 app.listen(config.environment.port, () =>
   console.log('~~Server running on port', config.environment.port)
