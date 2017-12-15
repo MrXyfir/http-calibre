f6f1d85c08141771781d359d73f9010a2319d789
MrXyfir <mr@xyfir.com>
2017-12-14 20:47:34 -0800

    Replace `cron` with `setInterval`

--- jobs/start-cron.js
+++ jobs/start-cron.js
@@ -1,5 +1,3 @@
-const cron = require('cron');
-
 // jobs
 const deleteExpiredDownloads = require('./delete-expired-downloads');
 
@@ -10,11 +8,6 @@ module.exports = function() {
 
   // Delete files in downloads directory that are over a day old
   // Runs twice a day
-  new cron.CronJob(
-    '0 1/12 * * *',
-    async () => await deleteExpiredDownloads(),
-    () => 1,
-    true
-  );
+  setInterval(deleteExpiredDownloads, 43200 * 1000);
 
 };
