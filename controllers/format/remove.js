c60b6af66f152b4c8b16359ce3f3308ba741f071
MrXyfir <mr@xyfir.com>
2017-12-15 11:37:33 -0800

    Updates and improvements
    - Update code style
    - Remove lib/resize-disk
    - Use node-calibre
    - Support Calibre v3
    - Add error message response

--- controllers/format/remove.js
+++ controllers/format/remove.js
@@ -1,33 +1,26 @@
-const resizeDisk = require("lib/resize-disk");
-const request = require("request");
-const escape = require("js-string-escape");
-const exec = require("child_process").exec;
+const Calibre = require('node-calibre');
 
 /*
-    DELETE libraries/:lib/books/:book/format/:format
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Deletes a specific format of :book
+  DELETE libraries/:lib/books/:book/format/:format
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Deletes a specific format of :book
 */
-module.exports = function(req, res) {
-    
-    if (!req.params.format.match(/^[A-Za-z0-9]{1,5}$/)) {
-        res.json({ error: true });
-        return;
-    }
-    
-    exec(
-        `calibredb remove_format --library-path ${req._path.lib} --dont-notify-gui ${+req.params.book} ${req.params.format}`,
-        (err, data, stderr) => {
-            if (err) {
-                res.json({ error: true });
-            }
-            else {
-                res.json({ error: false });
-                resizeDisk();
-            }
-        }
+module.exports = async function(req, res) {
+
+  const calibre = new Calibre({ library: req._path.lib });
+
+  try {
+    if (!req.params.format.match(/^[A-Za-z0-9]{1,5}$/))
+      throw 'Invalid format';
+
+    await calibre.run(
+      'calibredb remove_format', [+req.params.book, req.params.format]
     );
-    
+  }
+  catch (err) {
+    res.json({ error: true, message: err });
+  }
+
 };
