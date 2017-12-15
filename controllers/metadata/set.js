4cac048539be0d19ccb219c90ba8734ca9c61782
MrXyfir <mr@xyfir.com>
2017-12-15 11:38:35 -0800

    Updates and improvements
    - Update code style
    - Remove lib/resize-disk
    - Use node-calibre
    - Support Calibre v3
    - Replace request with superagent

--- controllers/metadata/set.js
+++ controllers/metadata/set.js
@@ -1,61 +1,44 @@
-const resizeDisk = require("lib/resize-disk");
-const request = require("request");
-const escape = require("js-string-escape");
-const exec = require("child_process").exec;
-
-const config = require("config");
+const Calibre = require('node-calibre');
+const request = require('superagent');
+const config = require('config');
 
 /*
-    PUT libraries/:lib/books/:book/metadata
-    REQUIRED
-        data: json-string
-            { key: value, key2: value, ... }
-    RETURN
-        { error: boolean, message?: string }
-    DESCRIPTION
-        Sets a field in book's metadata
-        Notify API of book's metadata change
+  PUT libraries/:lib/books/:book/metadata
+  REQUIRED
+    data: json-string
+      { key: value, key2: value, ... }
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Sets a field in book's metadata
+    Notify API of book's metadata change
 */
-module.exports = function(req, res) {
-    
-    let fields = {};
-    
-    try {
-        fields = JSON.parse(req.body.data);
-    }
-    catch (e) {
-        res.json({ error: true, message: "Bad fields" });
-        return;
-    }
-    
-    // Build / escape field arguments
-    const data = Object.keys(fields).map(field => {
-        return `-f "${escape(field)}:${escape(fields[field])}"`;
-    }).join(' ');
-    
-    exec(
-        `calibredb set_metadata --library-path ${req._path.lib} --dont-notify-gui ${data} ${+req.params.book}`,
-        (err, data, stderr) => {
-            if (err || data.indexOf("Backing up metadata") == -1) {
-                res.json({ error: true, message: "Could not set metadata" });
-            }
-            else {
-                res.json({ error: false });
+module.exports = async function(req, res) {
 
-                exec(
-                    `calibredb embed_metadata --library-path ${req._path.lib} --dont-notify-gui ${+req.params.book}`,
-                    (err, data, stderr) => {
-                        request.put({
-                            url: config.urls.api + req._libId
-                                + "/books/" + +req.params.book,
-                            form: { type: "metadata" } 
-                        }, (err, response, body) => 1);
-                    }
-                );
+  const calibre = new Calibre({ library: req._path.lib });
+  let fields = {};
 
-                resizeDisk();
-            }
-        }
-    );
-    
-};
+  try {
+    try { fields = JSON.parse(req.body.data); }
+    catch (err) { throw 'Bad fields'; }
+
+    for (let field in fields) {
+      const result = await calibre.run(
+        'calibredb set_metadata',
+        [+req.params.book],
+        { f: `${field}:${fields[field]}` }
+      );
+
+      if (result.indexOf('Backing up metadata') == -1)
+        throw 'Could not set metadata';
+    }
+
+    await calibre.run('calibredb embed_metadata', [+req.params.book]);
+
+    res.json({ error: false });
+  }
+  catch (err) {
+    res.json({ error: true, message: err });
+  }
+
+}
