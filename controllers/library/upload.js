4e9e9e8431f38bc643e88e91d19c813f8b758df7
MrXyfir <mr@xyfir.com>
2018-01-10 20:36:10 -0800

    Improvements, changes
    - Implement `lib/add-xy-columns`.
    - Update code style
    - Remove unused code

--- controllers/library/upload.js
+++ controllers/library/upload.js
@@ -1,4 +1,4 @@
-const sqlite = require('sqlite3');
+const addXyColumns = require('lib/add-xy-columns');
 const unzip = require('extract-zip');
 const fs = require('fs-extra');
 
@@ -11,39 +11,23 @@ const config = require('config');
   DESCRIPTION
     Empty previous library directory
     Unzip file into library folder
-    Grab all book ids from metadata.db
-    Send book ids to core API
+    Add `xy__` custom columns if needed
 */
 module.exports = async function(req, res) {
 
   try {
-    await new Promise((resolve, reject) => {
-      fs.emptyDir(req._path.lib, err => {
-        err ? reject('Could not wipe old library') : resolve();
-      });
-    });
+    await fs.emptyDir(req._path.lib);
 
     await new Promise((resolve, reject) => {
-      unzip(req.file.path, { dir: req._path.lib }, err => {
-        err ? reject('Could not unzip file') : resolve();
-      });
+      unzip(req.file.path, { dir: req._path.lib }, err =>
+        err ? reject('Could not unzip file') : resolve()
+      );
     });
 
-    const db = new sqlite.Database(req._path.lib + '/metadata.db');
-
-    const ids = await new Promise((resolve, reject) => {
-      db.all('SELECT id FROM "books"', (err, rows) => {
-        db.close();
-
-        err || !rows.length
-          ? reject('Invalid library')
-          : resolve(rows.map(row => row.id).join(','));
-      });
-    });
+    await addXyColumns(req._path.lib);
 
     res.json({ error: false });
-
-    fs.emptyDir(req._path.ul, () => 1);
+    await fs.emptyDir(req._path.ul);
   }
   catch (err) {
     res.json({ error: true, message: err });
