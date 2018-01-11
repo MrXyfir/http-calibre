f305eea3dd9349f77bacb6c0460e0babaf3742f8
MrXyfir <mr@xyfir.com>
2018-01-10 20:35:28 -0800

    Implement `lib/add-xy-columns`. Update code style

--- controllers/library/create.js
+++ controllers/library/create.js
@@ -1,30 +1,28 @@
-const fs = require("fs-extra");
+const addXyColumns = require('lib/add-xy-columns');
+const fs = require('fs-extra');
 
 /*
-    POST libraries/:lib
-    RETURN
-       { error: boolean }
-    DESCRIPTION
-        Create library and library's upload folder
+  POST libraries/:lib
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Create library and library's upload folder
 */
-module.exports = function(req, res) {
-    
-    fs.mkdir(req._path.lib, err => {
-        if (err) {
-            res.json({ error: true });
-        }
-        else {
-            fs.mkdir(req._path.ul, err => {
-               if (err) {
-                   fs.remove(req._path.lib, err => {
-                       res.json({ error: true });
-                   });
-               } 
-               else {
-                   res.json({ error: false });
-               }
-            });
-        }
-    });
-    
+module.exports = async function(req, res) {
+
+  try {
+    // Create library's storage and upload directories
+    await Promise.all([
+      fs.mkdir(req._path.lib),
+      fs.mkdir(req._path.ul)
+    ]);
+
+    await addXyColumns(req._path.lib);
+
+    res.json({ error: false });
+  }
+  catch (err) {
+    res.json({ error: true, message: err });
+  }
+
 };
