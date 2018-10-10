04a5a6b46bcf3826ee21aba5a6ebdf3a69e99d9f
MrXyfir <mr@xyfir.com>
2018-10-10 10:20:35 -0700

    Add default config

---
+++ config.default.js
@@ -0,0 +1,19 @@
+exports.directories = {
+  libraries: '',
+  downloads: '',
+  uploads: ''
+};
+exports.environment = {
+  runCronJobs: false,
+  port: 2085,
+  type: 'dev'
+};
+exports.keys = {
+  mailgun: ''
+};
+exports.urls = {
+  mailgun: {
+    domain: '',
+    api: ''
+  }
+};
